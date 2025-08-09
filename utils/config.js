5428f63f2c4427488d749478baf692399186a124
anhsayshello <anhle0347@gmail.com>
2025-08-09 21:23:26 +0700

    feat: add oauth2 google

--- utils/config.js
+++ utils/config.js
@@ -1,8 +1,21 @@
 import "dotenv/config";
+import { google } from "googleapis";
 
 const PORT = process.env.PORT;
 const MONGODB_URI = process.env.MONGODB_URI;
+const JWT_SECRET = process.env.JWT_SECRET;
 
-const SECRET = process.env.SECRET;
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 
-export default { PORT, MONGODB_URI, SECRET };
+export default {
+  PORT,
+  MONGODB_URI,
+  JWT_SECRET,
+};
+
+export const oauth2client = new google.auth.OAuth2(
+  GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET,
+  "postmessage"
+);
