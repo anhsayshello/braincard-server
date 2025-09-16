ff18a80595e03ee7bd53d5b3396bf605357f756a
anhsayshello <anhle0347@gmail.com>
2025-09-16 17:15:03 +0700

    fix: import module

--- functions/api.js
+++ functions/api.js
@@ -1,4 +1,4 @@
-import serverless from "serverless-http";
+import ServerlessHttp from "serverless-http";
 import app from "../index.js";
 
-export const handler = serverless(app);
+export const handler = ServerlessHttp(app);
